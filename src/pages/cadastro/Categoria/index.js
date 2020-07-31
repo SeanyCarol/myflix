5b9b21124d4f1986eb0b0d6f15433a460f0e0f02
SeanyCarol <seany@ice.ufjf.br>
2020-07-31 15:37:36 -0300

    Aula 4 finalizada com ajustes na URL local e de produção

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -28,7 +28,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://myflix-omega.herokuapp.com/categorias';
     fetch(URL)
       .then(async (respostaDoServer) => {
         if (respostaDoServer.ok) {
